1464e521832201f6396ccd00c24a77bafa62311f
longshihui <391424924@qq.com>
2019-07-18 18:07:00 +0800

    feat(table): 新增EventsTable、MethodsTable、PropsTable、SlotsTable

---
+++ src/components/MethodsTable.vue
@@ -0,0 +1,35 @@
+<script>
+import Table from './table'
+const columns = [
+    {
+        title: '方法名',
+        key: 'name'
+    },
+    {
+        title: '返回值',
+        key: 'returnValue'
+    },
+    {
+        title: '说明',
+        key: 'explain'
+    }
+]
+
+export default {
+    name: 'MethodsTable',
+    functional: true,
+    props: {
+        data: {
+            type: Array,
+            default: () => []
+        },
+        defaultValue: {
+            type: String,
+            default: '-'
+        }
+    },
+    render(h, ctx) {
+        return <Table columns={columns} data={ctx.props.data} />
+    }
+}
+</script>
---
+++ src/components/PropsTable.vue
@@ -0,0 +1,43 @@
+<script>
+import Table from './table'
+const columns = [
+    {
+        title: '属性',
+        key: 'prop'
+    },
+    {
+        title: '类型',
+        key: 'type'
+    },
+    {
+        title: '是否必传',
+        key: 'required'
+    },
+    {
+        title: '默认值',
+        key: 'default'
+    },
+    {
+        title: '说明',
+        key: 'explain'
+    }
+]
+
+export default {
+    name: 'PropsTable',
+    functional: true,
+    props: {
+        data: {
+            type: Array,
+            default: () => []
+        },
+        defaultValue: {
+            type: String,
+            default: '-'
+        }
+    },
+    render(h, ctx) {
+        return <Table columns={columns} data={ctx.props.data} />
+    }
+}
+</script>
