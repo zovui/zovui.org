a7283b7d3e568c6633c0c07805f99f56f0271725
longshihui <391424924@qq.com>
2019-07-18 14:48:08 +0800

    feat(layout): 新增layout文档

---
+++ src/examples/layout/TopBottom.vue
@@ -0,0 +1,42 @@
+<title>上下布局</title>
+
+<template>
+	<div class="demo">
+		<Layout class="container">
+			<Header></Header>
+			<Layout>
+				<Sider></Sider>
+				<Layout>
+					<Content></Content>
+					<Footer></Footer>
+				</Layout>
+			</Layout>
+		</Layout>
+	</div>
+</template>
+
+<style lang="scss">
+.demo {
+	$color-primary: #2d8cf0;
+	$layout-header-background: lighten($color-primary, 10%);
+	$layout-content-background: lighten($color-primary, 25%);
+	$layout-footer-background: $layout-header-background;
+	$layout-sider-background: $color-primary;
+	.zov-layout-header {
+		background: $layout-header-background;
+	}
+	.zov-layout-content {
+		background: $layout-content-background;
+	}
+	.zov-layout-sider {
+		background: $layout-sider-background;
+	}
+	.zov-layout-footer {
+		background: $layout-footer-background;
+	}
+}
+.container {
+	height: 400px;
+	margin-top: 15px;
+}
+</style>
