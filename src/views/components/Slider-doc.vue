6740362d3867ec75aafd38b002dc9edd4dcdb4f0
longshihui <391424924@qq.com>
2019-07-19 15:17:08 +0800

    feat(slider): 完善api文档

--- src/views/components/Slider-doc.vue
+++ src/views/components/Slider-doc.vue
@@ -21,11 +21,84 @@ const SliderApi = [
         data: [
             {
                 prop: 'value',
+                type: 'number',
+                required: 'false',
+                default: '0',
+                values: '-',
+                explain: 'Slider组件的值，可用v-model 双向绑定数据'
+            },
+            {
+                prop: 'min',
+                type: 'number',
+                required: 'false',
+                default: '0',
+                values: '-',
+                explain: 'Slider最小值'
+            },
+            {
+                prop: 'max',
+                type: 'number',
+                required: 'false',
+                default: '100',
+                values: '-',
+                explain: 'Slider最大值'
+            },
+            {
+                prop: 'tooltipVisible',
+                type: 'string',
+                required: 'false',
+                default: 'default',
+                values: ['default', 'hidden', 'always'].join(' | '),
+                explain:
+                    '控制点tooltip展示规则，default只有在拖拽时展示；hidden总是隐藏；always为总是展示'
+            },
+            {
+                prop: 'tipFormatter',
+                type: 'function',
+                required: 'false',
+                default: 'value => value.toString()',
+                values: '-',
+                explain: 'tooltip内容格式化'
+            },
+            {
+                prop: 'range',
                 type: 'boolean',
                 required: 'false',
                 default: 'false',
                 values: '-',
-                explain: '侧边栏是否收起，可使用 v-model 双向绑定数据'
+                explain: '是否启用范围选择模式'
+            },
+            {
+                prop: 'step',
+                type: 'number',
+                required: 'false',
+                default: '1',
+                values: '-',
+                explain: '步长'
+            },
+            {
+                prop: 'marks',
+                type: 'object',
+                required: 'false',
+                default: '-',
+                values: '-',
+                explain: '打标记'
+            },
+            {
+                prop: 'only-marks',
+                type: 'boolean',
+                required: 'false',
+                default: '-',
+                values: '-',
+                explain: '仅在设置marks时生效，是否只能拖拽到marks上'
+            },
+            {
+                prop: 'vertical',
+                type: 'boolean',
+                required: 'false',
+                default: '-',
+                values: '-',
+                explain: '是否使用垂直模式'
             }
         ]
     },
