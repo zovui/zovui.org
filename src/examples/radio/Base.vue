45edf5943ef7f5e9e4ef926a08897f88a3303c6a
longshihui <391424924@qq.com>
2019-07-22 16:50:22 +0800

    feat(radio): 新增基础使用文档

---
+++ src/examples/radio/Base.vue
@@ -0,0 +1,20 @@
+<title>基础使用</title>
+
+<template>
+    <article>
+        <section>当前值: {{ value }}</section>
+        <section>
+            <Radio name="aaa" v-model="value" size="large" label="1" />
+        </section>
+    </article>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: ''
+        }
+    }
+}
+</script>
--- src/views/components/Radio-doc.vue
+++ src/views/components/Radio-doc.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { ComponentExampleContainer, ComponentExample } from '@/components'
+import Base from '@/examples/radio/Base.vue?demo'
 
 const RadioApi = [
     {
@@ -134,7 +135,7 @@ export default {
     },
     data() {
         return {
-            examples: [],
+            examples: [Base],
             apiList: [...RadioApi, ...RadioGroupApi]
         }
     }
