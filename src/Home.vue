eacf657d6319d72dc3de69a6d05c2ed01dffdb3e
longshihui <391424924@qq.com>
2019-07-22 14:25:30 +0800

    feat(主页): 新增logo组件

--- src/Home.vue
+++ src/Home.vue
@@ -1,6 +1,35 @@
 <template>
-    <div id="home">
-        <div>首页</div>
-        <router-link to="/install">开始使用</router-link>
-    </div>
+    <article>
+        <section class="first-screen">
+            <div class="first-screen-mid">
+                <Logo />
+                <div>首页</div>
+                <router-link to="/install">开始使用</router-link>
+            </div>
+        </section>
+    </article>
 </template>
+
+<style lang="scss" scoped>
+.first-screen {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    &-mid {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -80%);
+    }
+}
+</style>
+
+<script>
+import Logo from '@/components/Logo'
+export default {
+    name: 'Home',
+    components: {
+        Logo
+    }
+}
+</script>
