028de39e4fe973fe2c18d30dcfe6f074492b9050
longshihui <391424924@qq.com>
2019-07-23 09:56:44 +0800

    feat(input): 新增文档

--- src/views/components/Input-doc.vue
+++ src/views/components/Input-doc.vue
@@ -1,131 +1,113 @@
 <template>
-    <div style="text-align: center">
-        <ul>
-            <b>属性</b>
-            <li>
-                大：<Input
-                    size="large"
-                    autofocus
-                    placeholder="这是一个自动获取焦点的input"
-                />
-            </li>
-            <li>
-                默认：<Input disabled placeholder="这是一个disabled的input" />
-            </li>
-            <li>
-                小：<Input
-                    size="small"
-                    clearable
-                    placeholder="这是一个可清除的input"
-                />
-            </li>
-        </ul>
-        <ul>
-            <b>功能</b>
-            一切vue对原生input支持的功能都将支持在ZovInput组件上
-            <li>
-                <Input
-                    v-model="inputV"
-                    @focus="$event.target.style.background = 'red'"
-                    @blur="$event.target.style.background = 'unset'"
-                    placeholder="这是测试focus、blur事件的input"
-                />
-            </li>
-            <li>
-                <Input
-                    v-model="inputV"
-                    @input="zov_input"
-                    placeholder="这是测试input事件的input"
-                />
-            </li>
-            <li>
-                <Input
-                    v-model="inputV"
-                    @change="
-                        $event.target.style.color =
-                            ['red', 'green', 'blue'][
-                                ((Math.random() * 10) / 2).toFixed()
-                            ] || 'green'
-                    "
-                    placeholder="这是测试change事件的input"
-                />
-            </li>
-        </ul>
-        <ul>
-            自定义图标
-            <li>
-                <Input
-                    v-model="inputV"
-                    prefix="contact"
-                    suffix="search"
-                    placeholder="输入用户名"
-                />
-            </li>
-        </ul>
-        <ul>
-            自定义功能
-            <li>
-                <Input
-                    block
-                    v-model="inputV"
-                    clearable
-                    suffix="search"
-                    placeholder="这是一个可清除,并加搜索的input"
-                />
-            </li>
-            <li>
-                <Input
-                    v-model="inputV"
-                    type="number"
-                    placeholder="这是一个加搜索按钮的input"
-                />
-            </li>
-            <li>
-                <Input
-                    v-model="inputV"
-                    clearable
-                    eye
-                    type="password"
-                    placeholder="这是一个可看的密码input"
-                />
-            </li>
-            slot
-            <li>
-                <Input
-                    v-model="inputV"
-                    clearable
-                    eye
-                    placeholder="这是一个可看的密码input"
-                >
-                    <template #prefix
-                        ><span>有</span></template
-                    >
-                    <template #suffix
-                        ><span>个</span></template
-                    >
-                </Input>
-            </li>
-        </ul>
-    </div>
+    <ComponentExampleContainer component-name="Input" :apiList="apiList">
+        <template #desc>
+            输入框；基本表单组件，在原生控件基础上进行了功能扩展。
+            一切vue对原生input支持的功能都将支持在ZovInput组件上。
+        </template>
+        <ComponentExample
+            v-for="example of examples"
+            :example="example"
+            :key="example.__title"
+        />
+    </ComponentExampleContainer>
 </template>
+
 <script>
+import { ComponentExampleContainer, ComponentExample } from '@/components'
+
+const InputApi = [
+    {
+        title: 'Input props',
+        type: 'props',
+        data: [
+            {
+                prop: 'value',
+                type: 'string|number',
+                required: 'true',
+                default: "''",
+                values: '-',
+                explain: '当前输入框的值，v-model时对应的值'
+            },
+            {
+                prop: 'type',
+                type: 'string',
+                required: 'false',
+                default: 'text',
+                values: 'html5支持的input类型',
+                explain: '指定输入框的类型'
+            },
+            {
+                prop: 'clearable',
+                type: 'boolean',
+                required: 'false',
+                default: 'false',
+                values: '-',
+                explain: '是否显示输入框清除按钮'
+            },
+            {
+                prop: 'eye',
+                type: 'boolean',
+                required: 'false',
+                default: 'false',
+                values: '-',
+                explain: '当输入框类型为password时，是否启用可见密码的交互'
+            },
+            {
+                prop: 'prefix',
+                type: 'string',
+                required: 'false',
+                default: "''",
+                values: '-',
+                explain: '输入框的前缀修饰'
+            },
+            {
+                prop: 'suffix',
+                type: 'string',
+                required: 'false',
+                default: "''",
+                values: '-',
+                explain: '输入框的后缀修饰'
+            }
+        ]
+    },
+    {
+        title: 'Input slots',
+        type: 'slots',
+        data: [
+            {
+                name: 'prefix',
+                explain: '输入框的前缀修饰'
+            },
+            {
+                name: 'suffix',
+                explain: '输入框的后缀修饰'
+            }
+        ]
+    },
+    {
+        title: 'Input events',
+        type: 'events',
+        data: [
+            {
+                event: 'input',
+                callbackParam: '当前输入框的值',
+                explain: '输入框值变化时触发'
+            }
+        ]
+    }
+]
+
 export default {
+    name: 'Input-doc',
+    components: {
+        ComponentExampleContainer,
+        ComponentExample
+    },
     data() {
         return {
-            inputV: ''
-        }
-    },
-    methods: {
-        zov_input(v) {
-            console.log(v)
+            examples: [],
+            apiList: [...InputApi]
         }
     }
 }
 </script>
-<style lang="scss" scoped>
-ul {
-    li {
-        margin: 5px 0;
-    }
-}
-</style>
