c077abb481e1a793efd83ef83f9fbb9c79779601
longshihui <391424924@qq.com>
2019-07-23 16:10:19 +0800

    feat(message): 新增基础使用文档

--- src/views/components/Message-doc.vue
+++ src/views/components/Message-doc.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { ComponentExampleContainer, ComponentExample } from '@/components'
+import Base from '@/examples/message/Base.vue?demo'
 
 const apiList = [
     {
@@ -80,7 +81,7 @@ export default {
     },
     data() {
         return {
-            examples: [],
+            examples: [Base],
             apiList
         }
     }
