fb0f028a7ca1ab6071da07e95c94475aafa893a7
longshihui <391424924@qq.com>
2019-07-23 15:00:59 +0800

    feat(select): 新增基础使用的例子

--- src/views/components/Select-doc.vue
+++ src/views/components/Select-doc.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { ComponentExampleContainer, ComponentExample } from '@/components'
+import Base from '@/examples/select/Base.vue?demo'
 
 const SelectApi = [
     {
@@ -152,7 +153,7 @@ export default {
     },
     data() {
         return {
-            examples: [],
+            examples: [Base],
             apiList: [...SelectApi]
         }
     }
