f1b015cdedd59a1cd203af07572f26d1bf2b46ce
longshihui <391424924@qq.com>
2019-07-25 15:27:37 +0800

    feat(主页): 完善BackgroundSymbol组件

--- src/pages/home/Background.vue
+++ src/pages/home/Background.vue
@@ -1,59 +1,21 @@
 <template>
-    <div class="background">
-        <svg class="background-svg" xmlns="http://www.w3.org/2000/svg">
-            <symbol id="O">
-                <circle
-                    cx="50"
-                    cy="50"
-                    r="37.5"
-                    stroke-width="25"
-                    stroke="rgba(223, 73, 78, 0.9)"
-                    fill="transparent"
-                ></circle>
-            </symbol>
-            <symbol id="L">
-                <path
-                    d="
-                    M 0 25
-                    L 0 87.5
-                    A 12.5 12.5 0 0 0 12.5 100
-                    L 75 100
-                    A 12.5 12.5 0 0 0 75 75
-                    L 25 75
-                    L 25 25
-                    A 12.5 12.5 0 0 0 0 25
-                    Z"
-                ></path>
-            </symbol>
-            <symbol id="C">
-                <path
-                    d="
-                    M 85.36 14.64
-                    A 50 50 0 1 0 85.36 85.36
-                    A 12.5 12.5 0 0 0 67.68 67.68
-                    A 25 25 0 1 1 67.68 32.32
-                    A 12.5 12.5 0 0 0 85.36 14.64
-                    Z"
-                ></path>
-            </symbol>
-        </svg>
-    </div>
+    <BackgroundCanvas>
+        <BackgroundSymbol symbol="L" color="red" bottom="30px" left="20%" />
+        <BackgroundSymbol symbol="O" color="blue" bottom="30px" left="40%" />
+        <BackgroundSymbol symbol="C" color="orange" bottom="30px" left="60%" />
+        <BackgroundSymbol symbol="O" color="green" bottom="30px" left="80%" />
+    </BackgroundCanvas>
 </template>
 
-<style lang="scss" scoped>
-.background {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-}
-.background-svg {
-    width: 100%;
-    height: 100%;
-}
-</style>
-
 <script>
+import BackgroundCanvas from './BackgroundCanvas'
+import BackgroundSymbol from './BackgroundSymbol'
+
 export default {
-    name: 'Background'
+    name: 'Background',
+    components: {
+        BackgroundCanvas,
+        BackgroundSymbol
+    }
 }
 </script>
---
+++ src/pages/home/BackgroundSymbol.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="background-symbol" :class="classes" :style="styles">
+        <svg class="background-symbol-svg">
+            <use
+                class="background-symbol-svg-user"
+                :xlink:href="'#' + symbol"
+            ></use>
+        </svg>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.background-symbol {
+    position: absolute;
+    .background-symbol-svg {
+        width: 100px;
+        height: 100px;
+    }
+    &--orange {
+        .background-symbol-svg-user {
+            fill: rgba(224, 127, 61, 0.9);
+            color: rgba(224, 127, 61, 0.9);
+        }
+    }
+    &--red {
+        .background-symbol-svg-user {
+            fill: rgba(223, 73, 78, 0.9);
+            color: rgba(223, 73, 78, 0.9);
+        }
+    }
+    &--green {
+        .background-symbol-svg-user {
+            fill: rgba(94, 188, 58, 0.9);
+            color: rgba(94, 188, 58, 0.9);
+        }
+    }
+    &--blue {
+        .background-symbol-svg-user {
+            fill: rgba(80, 132, 206, 0.9);
+            color: rgba(80, 132, 206, 0.9);
+        }
+    }
+}
+</style>
+
+<script>
+export default {
+    name: 'BackgroundSymbol',
+    props: {
+        symbol: {
+            type: String,
+            validator(symbol) {
+                return ['L', 'O', 'C'].indexOf(symbol) !== -1
+            }
+        },
+        top: String,
+        left: String,
+        bottom: String,
+        right: String,
+        color: {
+            type: String,
+            validator(color) {
+                return ['red', 'orange', 'blue', 'green'].indexOf(color) !== -1
+            }
+        }
+    },
+    computed: {
+        classes() {
+            const classes = []
+            classes.push('background-symbol--' + this.color)
+            return classes
+        },
+        styles() {
+            return {
+                top: this.top,
+                left: this.left,
+                bottom: this.bottom,
+                right: this.right
+            }
+        }
+    }
+}
+</script>
