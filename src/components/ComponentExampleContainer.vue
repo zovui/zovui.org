301f43cb428d5871967afffa9a3379abe0f31936
longshihui <391424924@qq.com>
2019-07-29 17:04:44 +0800

    refactor(component): 移除冗余代码

--- src/components/ComponentExampleContainer.vue
+++ src/components/ComponentExampleContainer.vue
@@ -23,7 +23,6 @@
 
 <script>
 import ComponentApi from './ComponentApi'
-import ComponentAnchor from './ComponentAnchor'
 import Document from './Document'
 import DocumentSection from './DocumentSection'
 
@@ -42,41 +41,8 @@ export default {
     },
     components: {
         ComponentApi,
-        ComponentAnchor,
         Document,
         DocumentSection
-    },
-    provide() {
-        return {
-            ComponentExample: {
-                addExampleAnchor: anchor => {
-                    this.anchorList[1].children.push(anchor)
-                },
-                addApiAnchor: anchor => {
-                    this.anchorList[2].children.push(anchor)
-                }
-            }
-        }
-    },
-    data() {
-        return {
-            anchorList: [
-                {
-                    title: '概述',
-                    href: '#GS'
-                },
-                {
-                    title: '代码示例',
-                    href: '#examples',
-                    children: []
-                },
-                {
-                    title: 'API',
-                    href: '#API',
-                    children: []
-                }
-            ]
-        }
     }
 }
 </script>
