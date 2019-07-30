e04dd56e04e1743b19e126084ed25c8d7ff68900
longshihui <391424924@qq.com>
2019-07-30 17:38:41 +0800

    fix(long-list): 修复文档中的描述位置错误的问题

--- src/views/components/LongList-doc.vue
+++ src/views/components/LongList-doc.vue
@@ -1,6 +1,6 @@
 <template>
     <ComponentExampleContainer component-name="LongList 长列表" :apiList="api">
-        <template>
+        <template #desc>
             LongList组件的作用是优化超长一维列表渲染性能问题，渲染窗口内的item，超出的不渲染<br />
             利用了 slot-scope
             将子组件数据回传到上层组件，所以子项上可以取到`props`字段。{item[当前项数据],index[当前项下标]}
