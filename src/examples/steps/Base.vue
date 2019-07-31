785a28dbc8694534cb3bcd098f862b7ae66a2e7e
longshihui <391424924@qq.com>
2019-07-31 15:31:29 +0800

    feat(steps): 新增基础使用文档

---
+++ src/examples/steps/Base.vue
@@ -0,0 +1,32 @@
+<title>基础使用</title>
+
+<template>
+    <article>
+        <Steps :current="current">
+            <Step title="已完成" content="这里是该步骤的描述信息"></Step>
+            <Step title="进行中" content="这里是该步骤的描述信息"></Step>
+            <Step title="待进行" content="这里是该步骤的描述信息"></Step>
+            <Step title="待进行" content="这里是该步骤的描述信息"></Step>
+        </Steps>
+        <Button type="primary" @click="next">Next step</Button>
+    </article>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            current: 1
+        }
+    },
+    methods: {
+        next() {
+            if (this.current === 3) {
+                this.current = 0
+            } else {
+                this.current += 1
+            }
+        }
+    }
+}
+</script>
--- src/views/components/Steps-doc.vue
+++ src/views/components/Steps-doc.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { ComponentExampleContainer, ComponentExample } from '@/components'
+import Base from '@/examples/steps/Base.vue?demo'
 
 const api = [
     {
@@ -118,7 +119,7 @@ export default {
     },
     data() {
         return {
-            examples: [],
+            examples: [Base],
             api
         }
     }
