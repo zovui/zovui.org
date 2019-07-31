92de6e1b6f4355a50d71c6e5f84d4ff99ea72cdd
longshihui <391424924@qq.com>
2019-07-31 10:15:38 +0800

    feat(upload): 新增基础使用文档

--- src/views/components/Upload-doc.vue
+++ src/views/components/Upload-doc.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { ComponentExampleContainer, ComponentExample } from '@/components'
+import Base from '@/examples/upload/Base.vue?demo'
 
 const api = [
     {
@@ -248,7 +249,7 @@ export default {
     },
     data() {
         return {
-            examples: [],
+            examples: [Base],
             api
         }
     }
