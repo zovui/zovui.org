5e93bde934369025600ce37bae7abe99d5d68ef6
wangqiang33 <wangqiang33@58ganji-corp.com>
2019-08-01 15:04:13 +0800

    feat: add anchor component

---
+++ src/examples/anchor/Base.vue
@@ -0,0 +1,20 @@
+<title>基础使用</title>
+
+<desc>
+    最简单的用法。
+</desc>
+
+<template>
+    <Anchor show-ink>
+        <AnchorLink href="#WuwPo6MhY" title="概述" />
+        <AnchorLink href="#Kg-8fdxp-q" title="代码示例">
+            <AnchorLink ref="#shFKi09j7H" title="基础使用" />
+            <AnchorLink href="UwFsxew4TZ" title="静态位置" />
+        </AnchorLink>
+        <AnchorLink href="#qq_I7iA48I" title="API">
+            <AnchorLink href="#dIcHpnPc61" title="Anchor props" />
+            <AnchorLink href="#mR1p0Iw3bJ" title="Anchor events" />
+            <AnchorLink href="#BmloNy1qtZ" title="AnchorLink props" />
+        </AnchorLink>
+    </Anchor>
+</template>
