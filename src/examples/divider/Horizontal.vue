fd31f279ee70b94a129863d91e89226f61a26527
wangqiang33 <wangqiang33@58ganji-corp.com>
2019-08-01 18:59:22 +0800

    feat: add divider component

---
+++ src/examples/divider/Horizontal.vue
@@ -0,0 +1,33 @@
+<title>水平分割线</title>
+
+<desc>
+    默认为水平分割线，可在中间加入文字。
+</desc>
+
+<template>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+            merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+            tamen, quo modo.
+        </p>
+        <Divider />
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+            merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+            tamen, quo modo.
+        </p>
+        <Divider>With Text</Divider>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+            merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+            tamen, quo modo.
+        </p>
+        <Divider dashed />
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+            merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+            tamen, quo modo.
+        </p>
+    </div>
+</template>
