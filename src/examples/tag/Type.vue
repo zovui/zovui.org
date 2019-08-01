4d40d3e179186a51981f5914b2a4e8d106fffe3f
longshihui <391424924@qq.com>
2019-08-01 16:25:55 +0800

    feat(tag): 新增类型文档

---
+++ src/examples/tag/Type.vue
@@ -0,0 +1,16 @@
+<title>类型</title>
+
+<desc>指定looks属性可以让标签呈现不同的样式</desc>
+
+<template>
+    <article>
+        <Tag>default</Tag>
+        <Tag looks="primary">primary</Tag>
+        <Tag looks="dashed">dashed</Tag>
+        <Tag looks="text">text</Tag>
+        <Tag looks="info">info</Tag>
+        <Tag looks="success">success</Tag>
+        <Tag looks="warning">warning</Tag>
+        <Tag looks="error">error</Tag>
+    </article>
+</template>
