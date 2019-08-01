92a60b126fc6a9af5c1e5e3f52a90d2d3b5be805
wangqiang33 <wangqiang33@58ganji-corp.com>
2019-08-01 18:03:02 +0800

    perf: 修改href

--- src/examples/anchor/Static.vue
+++ src/examples/anchor/Static.vue
@@ -6,15 +6,15 @@
 
 <template>
     <Anchor show-ink :affix="false">
-        <AnchorLink href="#WuwPo6MhY" title="概述" />
-        <AnchorLink href="#Kg-8fdxp-q" title="代码示例">
-            <AnchorLink ref="#shFKi09j7H" title="基础使用" />
-            <AnchorLink href="UwFsxew4TZ" title="静态位置" />
+        <AnchorLink href="#6c40bd24" title="概述" />
+        <AnchorLink href="#52a13c85" title="代码示例">
+            <AnchorLink ref="#56bf0c85" title="基础使用" />
+            <AnchorLink href="#73329c7f" title="静态位置" />
         </AnchorLink>
-        <AnchorLink href="#qq_I7iA48I" title="API">
-            <AnchorLink href="#dIcHpnPc61" title="Anchor props" />
-            <AnchorLink href="#mR1p0Iw3bJ" title="Anchor events" />
-            <AnchorLink href="#BmloNy1qtZ" title="AnchorLink props" />
+        <AnchorLink href="#1a3a6644" title="API">
+            <AnchorLink href="#41cda35b" title="Anchor props" />
+            <AnchorLink href="#355576e4" title="Anchor events" />
+            <AnchorLink href="#f71e6316" title="AnchorLink props" />
         </AnchorLink>
     </Anchor>
 </template>
