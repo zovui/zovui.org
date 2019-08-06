ea068bed5c7e0c61dabefb694d09c1944ef416be
longshihui <391424924@qq.com>
2019-08-06 15:07:54 +0800

    feat(tag): 新增api文档

--- src/views/components/Tag-doc.vue
+++ src/views/components/Tag-doc.vue
@@ -23,7 +23,71 @@ import NotText from '@/examples/tag/NotText.vue?demo'
 import Delete from '@/examples/tag/Delete.vue?demo'
 import Size from '@/examples/tag/Size.vue?demo'
 
-const api = []
+const api = [
+    {
+        title: 'Tag props',
+        type: 'props',
+        data: [
+            {
+                prop: 'looks',
+                type: 'string',
+                required: 'false',
+                default: 'default',
+                values:
+                    'default, primary, dashed, text, info, success, warning, error',
+                explain: '设置标签类型'
+            },
+            {
+                prop: 'shape',
+                type: 'string',
+                required: 'false',
+                default: 'default',
+                values: 'default, circle',
+                explain: '设置标签形状'
+            },
+            {
+                prop: 'loading',
+                type: 'boolean',
+                required: 'false',
+                default: 'false',
+                values: '-',
+                explain: '设置标签加载中状态'
+            },
+            {
+                prop: 'spinname',
+                type: 'string',
+                required: 'false',
+                default: 'loading',
+                values: 'loading, dbcircle',
+                explain: '加载中图标样式'
+            },
+            {
+                prop: 'iconname',
+                type: 'string',
+                required: 'false',
+                default: "''",
+                values: '-',
+                explain: '自定义图标名'
+            },
+            {
+                prop: 'disabled',
+                type: 'boolean',
+                required: 'false',
+                default: 'false',
+                values: '-',
+                explain: '禁用标签'
+            },
+            {
+                prop: 'tag',
+                type: 'string',
+                required: 'false',
+                default: 'button',
+                values: '-',
+                explain: '标签元素类型'
+            }
+        ]
+    }
+]
 
 export default {
     name: 'Tag-doc',
